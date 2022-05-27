4b26337cbcc8c73f9ef28bcfcb41992ed2fdf37a
vens8 <vensr.maddula@gmail.com>
2022-05-27 19:02:19 +0530

    Update en.js

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,13 +1,17 @@
 // English
 const lang = {
+    about: 'About Me',
     contact: 'Contact',
     born: 'Born',
     bornIn: 'in',
     experience: 'Experience',
     education: 'Education',
     skills: 'Skills',
+    languages: 'Languages',
     projects: 'Projects',
+    clicktoview: 'Click to view',
     contributions: 'Contributions',
-    about: 'About me'
+    hobbies: 'Hobbies',
+    additional: 'Additional Info'
 };
 export default lang;
